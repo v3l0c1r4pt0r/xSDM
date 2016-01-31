cb4fcec631370626e4a59333efdf2b0b5be37628
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2016-01-31 11:40:32 +0100

    Allow unpacking second and next files
    Add support for SDC files containing multiple files. This probably
    fully solves github issue #4.

--- src/main.c
+++ src/main.c
@@ -138,15 +138,6 @@ int main(int argc, char **argv)
 
     printf("[OK]\n");
 
-    //check if number of files might be more than one
-    if(header->headerSize > 1)
-    {
-        fprintf(stderr,
-                "Warning! This container contains more than one file. However "
-                "this program can only extract first one. Check for updates!\n"
-               );
-    }
-
     printf("Checking file integrity...\t");
 
     //count crc32
@@ -169,6 +160,7 @@ int main(int argc, char **argv)
         printf("[OK]\n");
 
     FileUnion *current = header->files;
+    off_t filestart = headerSize + 4;
     File *after = &header->files[header->headerSize].file;
     FileName *fn = (FileName*)after;
 
@@ -180,197 +172,211 @@ int main(int argc, char **argv)
     err = decryptData(&fn->fileName, &fnLength, data, unpackData.fileNameKey, 32);
     if(err != DD_OK)
     {
-      printf("[FAIL]");
-      fprintf(stderr, "%s: Error while decrypting file name (errorcode: %d)", argv[0], err);
-      return err;
+    printf("[FAIL]");
+    fprintf(stderr, "%s: Error while decrypting file name (errorcode: %d)", argv[0], err);
+    return err;
     }
     memcpy((void*)&fn->fileName,data, fnLength);
 
     printf("[OK]\n");
 
-    char *first_fn = &fn->fileName;
-    uint32_t fn_size = fnLength;
-    if(header->headerSize > 1)
+    // unpack files
+    int fileid;
+    for(fileid = 0; fileid < header->headerSize; fileid++)
     {
-        //TODO: allow unpacking second and next files
-        FileUnion *second = &header->files[1];
-        fn_size = second->file.fileNameOffset;
-        first_fn = (char*)malloc(fn_size);
-        bzero(first_fn, fn_size);
-        memcpy(first_fn, data, fn_size - 1);
-    }
+        char *filename = (char*)(&fn->fileName);
+        filename += current->file.fileNameOffset;
+        uint32_t fn_size = strlen(filename);
+//         if(header->headerSize > 1)
+//         {
+//             //TODO: allow unpacking second and next files
+//             FileUnion *second = &header->files[1];
+//             fn_size = second->file.fileNameOffset;
+//             first_fn = (char*)malloc(fn_size);
+//             bzero(first_fn, fn_size);
+//             memcpy(first_fn, data, fn_size - 1);
+//         }
 
-    if(flags & F_VERBOSE)
-        fprintf(stderr,"File path: %s\n",first_fn);
+        if(flags & F_VERBOSE)
+            fprintf(stderr,"File path: %s\n",filename);
 
-    printf("Creating directory structure...\t");
+        printf("Creating directory structure...\t");
 
-    dosPathToUnix(first_fn);
+        dosPathToUnix(filename);
 
-    void *dirName = malloc(fn_size);
-    strncpy((char*)dirName,first_fn,fn_size - 1);
-    dirName = dirname((char*)dirName);
+        void *dirName = malloc(fn_size);
+        strncpy((char*)dirName,filename,fn_size - 1);
+        dirName = dirname((char*)dirName);
 
-    char *baseName = basename(first_fn);
+        char *baseName = basename(filename);
 
-    //get sdc location
-    char *sdcDir = (char*)malloc(strlen(sdcFile)+1);
-    strcpy(sdcDir,sdcFile);
-    sdcDir = dirname(sdcDir);
+        //get sdc location
+        char *sdcDir = (char*)malloc(strlen(sdcFile)+1);
+        strcpy(sdcDir,sdcFile);
+        sdcDir = dirname(sdcDir);
 
-    //create directory according to header
-    char *outFile = (char*)malloc(strlen(sdcDir)+strlen((char*)dirName)+2);
-    sprintf(outFile,"%s/%s",sdcDir,(char*)dirName);
-    int ret = createDir(outFile);
-    if(ret != 0)
-    {
-        printf("[FAIL]\n");
-        fprintf(stderr,"%s: Directory '%s' creation failed with errno: %d\n",argv[0], outFile,errno);
-    }
-
-    printf("[OK]\n");
-
-    if(flags & F_VERBOSE)
-    {
-#define TIMESIZE	20
-      char crtime[TIMESIZE];
-      time_t creation = winTimeToUnix(current->file.creationTime);
-      unixTimeToStr(crtime, TIMESIZE, creation);
-
-      char actime[TIMESIZE];
-      time_t access = winTimeToUnix(current->file.accessTime);
-      unixTimeToStr(actime, TIMESIZE, access);
-
-      char mdtime[TIMESIZE];
-      time_t modification = winTimeToUnix(current->file.modificationTime);
-      unixTimeToStr(mdtime, TIMESIZE, modification);
-
-      fprintf(stderr, "File has been originally created at %s, last accessed at %s and modified at %s\n", crtime, actime, mdtime);
-    }
-
-    printf("Unpacking file(s)...\t\t");
-
-    //open output file
-    outFile = (char*)realloc(outFile, strlen(sdcDir)+strlen((char*)dirName)+strlen(baseName)+3);
-    sprintf(outFile,"%s/%s/%s",sdcDir,(char*)dirName,baseName);
-    FILE *out = fopen(outFile,"w");
-    if(out == NULL)
-    {
-        //error opening a file
-        printf("[FAIL]\n");
-        perror(outFile);
-        return errno;
-    }
-
-    //memory cleanup
-    free(outFile);
-    outFile = NULL;
-    //free(sdcDir);//FIXME: SIGABRT
-    sdcDir = NULL;
-    free(dirName);
-    dirName = NULL;
-
-    //ensure we are after header
-    int r;
-    if((r = fseek(in,headerSize+4,SEEK_SET))!=0)
-        return r;
-
-    //create inflate struct
-    z_stream stream;
-    stream.next_in = Z_NULL;
-    stream.avail_in = 0;
-    stream.zalloc = Z_NULL;
-    stream.zfree = Z_NULL;
-    stream.opaque = Z_NULL;
-
-    //initialize stream
-    r = (int)-1;
-    if(header->headerSignature == SIG_ELARGE)
-        r = inflateInit(&stream);
-    else
-        r = inflateInit2_(&stream,-15,ZLIB_VERSION,(int)sizeof(z_stream));
-    if(r != Z_OK)
-    {
-        printf("[FAIL]\n");
-        fprintf(stderr,"inflateInit failed with errorcode %d (%s)\n",r,stream.msg);
-        return r;
-    }
-    //read from file
-    unsigned int bytesToRead;
-    if(header->headerSignature == SIG_ELARGE)
-    {
-        bytesToRead = current->file4gb.compressedSize & 0x3fff;
-    }
-    else
-    {
-        bytesToRead = current->file.compressedSize & 0x3fff;
-    }
-    unsigned char *input = (unsigned char*)malloc(bytesToRead);
-    unsigned char *output = (unsigned char*)malloc(0x4000);
-    void *tmp = malloc(bytesToRead);
-
-    //determine file size
-    unsigned int bytesRemaining = 0;
-    if(header->headerSignature == SIG_ELARGE)
-        bytesRemaining = current->file4gb.fileSize;
-    else
-        bytesRemaining = current->file.fileSize;
-
-    if(flags & F_VERBOSE)
-        fprintf(stderr,"file size has been set as %u (0x%04X), signature: 0x%02X\n",bytesRemaining,bytesRemaining,header->headerSignature);
-
-    while(bytesRemaining != 0)
-    {
-        result = fread(input+stream.avail_in,1,bytesToRead-stream.avail_in,in);
-        if(result == 0 && stream.avail_in == 0)	//stop only if stream iflated whole previous buffer
-            return 1;				//still have bytes remaining but container end reached
-
-        //decode
-        stream.next_in = (Bytef*)input;
-        stream.avail_in += result;
-        stream.next_out = (Bytef*)output;
-        stream.avail_out = 0x4000;
-        stream.total_in = 0;
-        stream.total_out = 0;
-        r = inflate(&stream,0);
-        if(r < Z_OK)
+        //create directory according to header
+        char *outFile = (char*)malloc(strlen(sdcDir)+strlen((char*)dirName)+2);
+        sprintf(outFile,"%s/%s",sdcDir,(char*)dirName);
+        int ret = createDir(outFile);
+        if(ret != 0)
         {
             printf("[FAIL]\n");
-            fprintf(stderr,"inflate failed with errorcode %d (%s)\n",r,stream.msg);
-            return r;
+            fprintf(stderr,"%s: Directory '%s' creation failed with errno: %d\n",argv[0], outFile,errno);
         }
 
-        //XOR
-        xorBuffer(unpackData.xorVal % 0x100, output, stream.total_out);
-
-        //write to file
-        fwrite(output,1,stream.total_out,out);
-        bytesRemaining -= stream.total_out;
-
-        /*
-        * tricky part: input buffer hadn't been fully decompressed
-        * so we need to copy the rest to TMP and then at the beginning
-        * of input buffer so it can be inflated, but before that we need to
-        * read the rest of a chunk so its size would be COMPRESSEDSIZE
-        */
-        memcpy(tmp,stream.next_in,stream.avail_in);
-        memcpy(input,tmp,stream.avail_in);
-    }
-
-    if(bytesRemaining != 0)
-    {
-        printf("[FAIL]\n");
-        fprintf(stderr, "%s: Unexpected end of file!\n", argv[0]);
-    }
-    else
         printf("[OK]\n");
 
-    free(tmp);
-    tmp = NULL;
-    free(input);
-    input = NULL;
-    free(output);
-    output = NULL;
+        if(flags & F_VERBOSE)
+        {
+#define TIMESIZE	20
+        char crtime[TIMESIZE];
+        time_t creation = winTimeToUnix(current->file.creationTime);
+        unixTimeToStr(crtime, TIMESIZE, creation);
+
+        char actime[TIMESIZE];
+        time_t access = winTimeToUnix(current->file.accessTime);
+        unixTimeToStr(actime, TIMESIZE, access);
+
+        char mdtime[TIMESIZE];
+        time_t modification = winTimeToUnix(current->file.modificationTime);
+        unixTimeToStr(mdtime, TIMESIZE, modification);
+
+        fprintf(stderr, "File has been originally created at %s, last accessed at %s and modified at %s\n", crtime, actime, mdtime);
+        }
+
+        printf("Unpacking file(s)...\t\t");
+
+        //open output file
+        outFile = (char*)realloc(outFile, strlen(sdcDir)+strlen((char*)dirName)+strlen(baseName)+3);
+        sprintf(outFile,"%s/%s/%s",sdcDir,(char*)dirName,baseName);
+        FILE *out = fopen(outFile,"w");
+        if(out == NULL)
+        {
+            //error opening a file
+            printf("[FAIL]\n");
+            perror(outFile);
+            return errno;
+        }
+
+        //memory cleanup
+        free(outFile);
+        outFile = NULL;
+        //free(sdcDir);//FIXME: SIGABRT
+        sdcDir = NULL;
+        free(dirName);
+        dirName = NULL;
+
+        //ensure we are after header
+        int r;
+        if((r = fseek(in,filestart,SEEK_SET))!=0)
+            return r;
+
+        //create inflate struct
+        z_stream stream;
+        stream.next_in = Z_NULL;
+        stream.avail_in = 0;
+        stream.zalloc = Z_NULL;
+        stream.zfree = Z_NULL;
+        stream.opaque = Z_NULL;
+
+        //initialize stream
+        r = (int)-1;
+        if(header->headerSignature == SIG_ELARGE)
+            r = inflateInit(&stream);
+        else
+            r = inflateInit2_(&stream,-15,ZLIB_VERSION,(int)sizeof(z_stream));
+        if(r != Z_OK)
+        {
+            printf("[FAIL]\n");
+            fprintf(stderr,"inflateInit failed with errorcode %d (%s)\n",r,stream.msg);
+            return r;
+        }
+        //read from file
+        unsigned int bytesToRead;
+        if(header->headerSignature == SIG_ELARGE)
+        {
+            bytesToRead = current->file4gb.compressedSize & 0x3fff;
+        }
+        else
+        {
+            bytesToRead = current->file.compressedSize & 0x3fff;
+        }
+        unsigned char *input = (unsigned char*)malloc(bytesToRead);
+        unsigned char *output = (unsigned char*)malloc(0x4000);
+        void *tmp = malloc(bytesToRead);
+
+        //determine file size
+        unsigned int bytesRemaining = 0;
+        if(header->headerSignature == SIG_ELARGE)
+            bytesRemaining = current->file4gb.fileSize;
+        else
+            bytesRemaining = current->file.fileSize;
+
+        if(flags & F_VERBOSE)
+            fprintf(stderr,"file size has been set as %u (0x%04X), signature: 0x%02X\n",bytesRemaining,bytesRemaining,header->headerSignature);
+
+        while(bytesRemaining != 0)
+        {
+            result = fread(input+stream.avail_in,1,bytesToRead-stream.avail_in,in);
+            if(result == 0 && stream.avail_in == 0)	//stop only if stream iflated whole previous buffer
+                return 1;				//still have bytes remaining but container end reached
+
+            //decode
+            stream.next_in = (Bytef*)input;
+            stream.avail_in += result;
+            stream.next_out = (Bytef*)output;
+            stream.avail_out = 0x4000;
+            stream.total_in = 0;
+            stream.total_out = 0;
+            r = inflate(&stream,0);
+            if(r < Z_OK)
+            {
+                printf("[FAIL]\n");
+                fprintf(stderr,"inflate failed with errorcode %d (%s)\n",r,stream.msg);
+                return r;
+            }
+
+            //XOR
+            xorBuffer(unpackData.xorVal % 0x100, output, stream.total_out);
+
+            //write to file
+            fwrite(output,1,stream.total_out,out);
+            bytesRemaining -= stream.total_out;
+
+            /*
+            * tricky part: input buffer hadn't been fully decompressed
+            * so we need to copy the rest to TMP and then at the beginning
+            * of input buffer so it can be inflated, but before that we need to
+            * read the rest of a chunk so its size would be COMPRESSEDSIZE
+            */
+            memcpy(tmp,stream.next_in,stream.avail_in);
+            memcpy(input,tmp,stream.avail_in);
+        }
+
+        if(bytesRemaining != 0)
+        {
+            printf("[FAIL]\n");
+            fprintf(stderr, "%s: Unexpected end of file!\n", argv[0]);
+        }
+        else
+            printf("[OK]\n");
+
+        fclose(out);
+        free(tmp);
+        tmp = NULL;
+        free(input);
+        input = NULL;
+        free(output);
+        output = NULL;
+
+        if(header->headerSignature == SIG_ELARGE)
+            filestart += current->file4gb.compressedSize;
+        else
+            filestart += current->file.compressedSize;
+        current++;
+    }
+
     free(unpackData.unformatted);
     unpackData.unformatted = NULL;
     unpackData.fileNameKey = NULL;
@@ -393,6 +399,5 @@ int main(int argc, char **argv)
     free(header);
 
     fclose(in);
-    fclose(out);
     return 0;
 }
